dc3813abe4cb90bb283a4da0dc1ceaea7f21fc87
Lariba Cloud <stecouto@proton.me>
2026-07-25 11:38:00 +0100

    fix: replace default Nextra metadata (#3)

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,10 +3,33 @@ import type { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Lariba Cloud</span>,
-  project: { link: 'https://github.com/laribacloud' },
-  docsRepositoryBase: 'https://github.com/laribacloud/lariba-docs-site/blob/main',
-  sidebar: { defaultMenuCollapseLevel: 1 },
-  footer: { text: `MIT ${new Date().getFullYear()} © Lariba Cloud.` }
+
+  project: {
+    link: 'https://github.com/laribacloud'
+  },
+
+  docsRepositoryBase:
+    'https://github.com/laribacloud-dev/lariba-docs-site/blob/main',
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s | Lariba Cloud Docs',
+      description:
+        'Official developer documentation for the Lariba Cloud operational event control plane.',
+      openGraph: {
+        type: 'website',
+        siteName: 'Lariba Cloud Docs'
+      }
+    }
+  },
+
+  sidebar: {
+    defaultMenuCollapseLevel: 1
+  },
+
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © Lariba Cloud.`
+  }
 }
 
 export default config
